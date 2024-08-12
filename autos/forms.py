96f3c3c11a04bc621899a3ca9833b9f0edb3d101
Agostina <agosqdo@gmail.com>
2024-08-11 22:46:51 -0300

    TASK-1: Improved visuals

--- autos/forms.py
+++ autos/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from .models import Car, Comment, Review
+from django.views.generic import CreateView
 
 class CarForm(forms.ModelForm):
     class Meta:
@@ -24,3 +25,16 @@ class CustomLoginForm(AuthenticationForm):
 class CustomRegistrationForm(UserCreationForm):
     # Add custom fields or configurations if necessary
     pass
+
+class ReviewCreateView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'review_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.car = get_object_or_404(Car, pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('car_detail', kwargs={'pk': self.object.car.pk})
